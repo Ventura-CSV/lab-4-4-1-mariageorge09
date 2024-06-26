ebf9a668113ad1f9eee7a19cb7a7a0675b7b9159
Maria <emailtomariageorge@gmail.com>
2024-06-26 16:44:10 -0700

    Exercise 4-1

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
-    print(number)
-
+    result = False
+    while result == False:
+        num = int(input('Enter your input: '))
+        if num > 0 and num <100:
+            number = num
+            result = True
+        else:
+            result = False 
+    print('Output: ')
+    print(number)    
+    return (result)
     ########################################
     # Do not delete the return statement
     ########################################
-    return number
-
 
 if __name__ == '__main__':
     main()
